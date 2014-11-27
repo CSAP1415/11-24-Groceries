093cc660fcddd219e3e3fa9108dd3557422d908d
CSAP1415 <phxbains@yahoo.com>
2014-11-27 00:26:46 -0700

    Groceries

--- src/Groceries.java
+++ src/Groceries.java
@@ -1,1 +1,18 @@
 // main program to run the GroceryList
+import java.util.*;
+public class Groceries
+{
+  public static void main(String[] args)
+  {
+    GroceryList list  = new Grocerylist();
+    list.add(new GroceryItem("Oreos",3,2.5));
+    list.add(new GroceryItem("Goldfish Crackers", 2, 1.75));
+    list.add(new GroceryItem("Apple", 8, .23));
+    list.add(new GroceryItem("Fruit Loops", 2, 4.32));
+    list.add(new GroceryItem("Pop Tarts", 4, 3.67));
+    System.out.println("Grocery List");
+    System.out.println(list.toString());
+    System.out.println(list.getTotalCost());
+  }
+}
+                  
